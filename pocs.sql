796256968b344dc935dc31e791468c8b177942c9
Hasrcharan Singh <artisangang@gmail.com>
2016-09-10 10:40:07 -0700

    updated db schema

--- pocs.sql
+++ pocs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2016 at 06:45 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.25
+-- Generation Time: Sep 10, 2016 at 07:39 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `pocs`
@@ -26,13 +26,21 @@ SET time_zone = "+00:00";
 -- Table structure for table `chat_history`
 --
 
-CREATE TABLE IF NOT EXISTS `chat_history` (
-`id` int(11) NOT NULL,
-  `receiver_id` int(10) unsigned NOT NULL COMMENT 'r',
-  `sender_id` int(10) unsigned NOT NULL,
+CREATE TABLE `chat_history` (
+  `id` int(11) NOT NULL,
+  `receiver_id` int(10) UNSIGNED NOT NULL COMMENT 'r',
+  `sender_id` int(10) UNSIGNED NOT NULL,
   `message` mediumtext NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chat_history`
+--
+
+INSERT INTO `chat_history` (`id`, `receiver_id`, `sender_id`, `message`, `created_at`) VALUES
+(1, 1, 2, 'hello', '2016-09-10 13:04:45'),
+(2, 2, 1, 'Hi how are you?', '2016-09-10 13:04:57');
 
 -- --------------------------------------------------------
 
@@ -40,22 +48,23 @@ CREATE TABLE IF NOT EXISTS `chat_history` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) unsigned NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
+  `client_id` varchar(40) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`) VALUES
-(1, 'harry', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'harry@yopmail.com', '2016-09-09 13:11:33'),
-(2, 'nitin', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'nitin@yopmail.com', '2016-09-09 13:11:33'),
-(3, 'aman', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'aman@yopmail.com', '2016-09-09 13:12:11');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `client_id`, `created_at`) VALUES
+(1, 'harry', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'harry@yopmail.com', 'C57d4437d9306f', '2016-09-09 13:11:33'),
+(2, 'nitin', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'nitin@yopmail.com', 'C57d4438629754', '2016-09-09 13:11:33'),
+(3, 'aman', '$2y$10$wCscaY9amNr1lxj4aCWgWO9B05G1Q/.jNjkU7j/Ov9j8diE.yJdcu', 'aman@yopmail.com', 'C57d440588c756', '2016-09-09 13:12:11');
 
 --
 -- Indexes for dumped tables
@@ -65,13 +74,16 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`) VALUES
 -- Indexes for table `chat_history`
 --
 ALTER TABLE `chat_history`
- ADD PRIMARY KEY (`id`), ADD KEY `chat_history_receiver_id_fk` (`receiver_id`), ADD KEY `chat_history_sender_id_fk` (`sender_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `chat_history_receiver_id_fk` (`receiver_id`),
+  ADD KEY `chat_history_sender_id_fk` (`sender_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`,`email`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`,`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -81,12 +93,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `chat_history`
 --
 ALTER TABLE `chat_history`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -95,8 +107,8 @@ MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 -- Constraints for table `chat_history`
 --
 ALTER TABLE `chat_history`
-ADD CONSTRAINT `chat_history_receiver_id_fk` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-ADD CONSTRAINT `chat_history_sender_id_fk` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `chat_history_receiver_id_fk` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `chat_history_sender_id_fk` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
